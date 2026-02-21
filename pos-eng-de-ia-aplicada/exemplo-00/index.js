f01192d4cf4377196469cadcf2a8bf65c3345f3f
Rebeca Morais <rebecamorais.12@gmail.com>
2026-02-21 15:08:06 -0300

    feat: Add `trainModel` function to define, compile, and train a TensorFlow.js sequential model.

--- pos-eng-de-ia-aplicada/exemplo-00/index.js
+++ pos-eng-de-ia-aplicada/exemplo-00/index.js
@@ -7,13 +7,44 @@ import tf from '@tensorflow/tfjs-node';
 //     { nome: "Carlos", idade: 40, cor: "verde", localizacao: "Curitiba" }
 // ];
 
-// Vetores de entrada com valores já normalizados e one-hot encoded
-// Ordem: [idade_normalizada, azul, vermelho, verde, São Paulo, Rio, Curitiba]
-// const tensorPessoas = [
-//     [0.33, 1, 0, 0, 1, 0, 0], // Erick
-//     [0, 0, 1, 0, 0, 1, 0],    // Ana
-//     [1, 0, 0, 1, 0, 0, 1]     // Carlos
-// ]
+async function trainModel(inputXs, outputYs) {
+    const model = tf.sequential();
+
+    model.add(tf.layers.dense({
+        inputShape: [7],        // 7 posicoes: [idade_normalizada, azul, vermelho, verde, São Paulo, Rio, Curitiba]
+        units: 80,              // 80 neuronios pq a gnt tem pouca base de treino
+        activation: 'relu'      // filtro de ativação - (Rectified Linear Unit) é uma função de ativação não linear que retorna o valor de entrada se for positivo, e 0 caso contrário 
+    }));
+
+    model.add(tf.layers.dense({
+        units: 3,                       // Camada de saida com 3 neuronios pq a gnt tem 3 categorias (premium, medium, basic)
+        activation: 'softmax'           // softmax para classificacao
+    }));
+
+    model.compile({
+        optimizer: 'adam',              // otimizador - Adaptive Moment Estimation - ajusta os pesos  (aprende com o historico de erros e acertos)
+        loss: 'categoricalCrossentropy', // compara a probabilidade prevista (calculo q o modelo achou) com a probabilidade real (resposta certa)
+        metrics: ['accuracy']           // para avaliar o desempenho do modelo - se a taxa de erro for mto alta, o modelo não aprendeu nada etc
+        // A resposta certa sempre vai ser uma entre as categorias possiveis. Ex: [1, 0, 0] ou [0, 1, 0] ou [0, 0, 1]
+        // O modelo vai tentar prever a resposta certa, comparando com a resposta certa, ele vai ajustar os pesos
+    });
+
+    // Treinamento do modelo
+    await model.fit(
+        inputXs,
+        outputYs,
+        {
+            verbose: 0,    // 0 = não mostra nada, 1 = mostra o progresso, 2 = mostra o progresso detalhado
+            epochs: 100,   // qtd de vezes q roda na lista de input 
+            shuffle: true, // embaralha os dados a cada época, pra não viciar o modelo em uma ordem específica de entrada dos dados
+            callbacks: {
+                // onEpochEnd: (epoch, logs) => {
+                //     console.log(`Epoch ${epoch + 1}: loss = ${logs.loss}, accuracy = ${logs.acc}`);
+                // }
+            }
+        });
+    return model;
+}
 
 // Usamos apenas os dados numéricos, como a rede neural só entende números.
 // tensorPessoasNormalizado corresponde ao dataset de entrada do modelo.
@@ -36,5 +67,4 @@ const tensorLabels = [
 const inputXs = tf.tensor2d(tensorPessoasNormalizado)
 const outputYs = tf.tensor2d(tensorLabels)
 
-inputXs.print();
-outputYs.print();
+const models = trainModel(inputXs, outputYs);
